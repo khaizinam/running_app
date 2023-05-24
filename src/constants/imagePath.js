74732c517d8065f6c8cde1771bb15feda79a7daf
khaizinam <khai.nguyenhuu@hcmut.edu.vn>
2023-05-24 23:43:59 +0700

    update

--- src/constants/imagePath.js
+++ src/constants/imagePath.js
@@ -2,5 +2,4 @@ export default {
     icCurLoc: require('../assets/images/Oval.png'),
     icGreenMarker: require("../assets/images/greenMarker.png"),
     greenIndicator: require("../assets/images/greenIndicator.png"),
-    icBike: require("../assets/images/bike.png"),
 }
