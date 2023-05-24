7c4627e474fba274b0270c4a50d7ce8093bfaacf
khaizinam <khai.nguyenhuu@hcmut.edu.vn>
2023-05-24 23:49:55 +0700

    update

--- App.js
+++ App.js
@@ -4,15 +4,28 @@ import {NavigationContainer} from '@react-navigation/native';
 //Screens
 import ChooseLocation from './src/Screens/ChooseLocation';
 import Home from './src/Screens/Home';
-
+import Welcome from './src/Screens/Welcome';
+import Onboarding1 from './src/Screens/Onboarding1';
+import Onboarding2 from './src/Screens/Onboarding2';
+import Onboarding3 from './src/Screens/Onboarding3';
+import Login from './src/Screens/Login';
+import Register from './src/Screens/Register';
+import AreaSelection from './src/Screens/AreaSelection';
 
 const App = () => {
   const Stack = createNativeStackNavigator ();
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="home" component={Home} />
-        <Stack.Screen name="chooseLocation" component={ChooseLocation} />
+        <Stack.Screen name="Welcome" component={Welcome} options={{headerShown:false}} />
+        <Stack.Screen name="Onboarding1" component={Onboarding1} />
+        <Stack.Screen name="Onboarding2" component={Onboarding2} />
+        <Stack.Screen name="Onboarding3" component={Onboarding3} />
+        <Stack.Screen name="Login" component={Login} options={{headerShown:false}} />
+        <Stack.Screen name="Register" component={Register} options={{headerShown:false}}/>
+        <Stack.Screen name="AreaSelection" component={AreaSelection} options={{headerShown:false}}/>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ChooseLocation" component={ChooseLocation} />
       </Stack.Navigator>
     </NavigationContainer>
   );
